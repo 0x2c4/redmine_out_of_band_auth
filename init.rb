9c8e9425f1ce16efc4df6a9077e7c2c455dadda0
seven <michael@mac4u.tech>
2021-02-11 08:48:04 +0000

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,11 +1,10 @@
 Redmine::Plugin.register :redmine_out_of_band_auth do
   name 'Redmine Out of Band Authentication plugin'
-  author 'Matsukei Co.,Ltd'
+  author 'Matsukei Co.,Ltd & 0x2c4'
   description 'Redmine plugin that provides Out of Band authentication by email.'
-  version '1.0.0'
-  requires_redmine version_or_higher: '3.2.0'
-  url 'https://github.com/matsukei/redmine_out_of_band_auth'
-  author_url 'http://www.matsukei.co.jp/'
+  version '1.0.1'
+  requires_redmine version_or_higher: '4.0.0'
+  url 'https://github.com/0x2c4/redmine_out_of_band_auth'
 end
 
 require_relative 'lib/out_of_band_auth'
